5209f2ecd6a1b74549bd023386725fe5ff20c09c
Slava <pvv56@mail.ru>
2022-05-07 14:35:53 +0300

    сделал кнопку навигации

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+navToggle.addEventListener('click', function() {
+  navToggle.classList.toggle('main-header__toggle--close');
+  navToggle.classList.toggle('main-header__toggle--open');
+});
