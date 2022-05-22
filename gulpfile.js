4ccb2c4935a5e8479626a85d714e675c6a59f833
Slava <52457249+slava560730@users.noreply.github.com>
2022-05-22 13:03:36 +0500

    Автоматизация сборки личного проекта (#11)
    * Сделал домашку
    
    * Удалил папку
    
    * исправил билд
    
    * Исправил сборку билда
    
    * Доделал все
    
    * поправил письмо

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import del from 'del';
 import browser from 'browser-sync';
 
 // Styles
@@ -12,34 +20,172 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+//html
+
+const html = () => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin())
+  .pipe(gulp.dest('build'));
+}
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src('source/js/*.js')
+  .pipe(terser())
+  .pipe(rename('script.min.js'))
+  .pipe(gulp.dest('build/js'))
+  .pipe(browser.stream());
+  }
+
+// Images
+
+const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/img'));
+  }
+
+  const optimizeImages2 = () => {
+    return gulp.src('source/img/favicons/*.svg')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img/favicons/'));
+    }
+
+const copyImages = () => {
+    return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(gulp.dest('build/img'));
+    }
+
+const copyImages2 = () => {
+      return gulp.src('source/img/favicons/*.svg')
+      .pipe(gulp.dest('build/img/favicons/'));
+      }
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+  .pipe(squoosh({
+  webp: {}
+  }))
+  .pipe(gulp.dest('build/img'))
+  }
+
+  // SVG
+
+const svg = () =>
+gulp.src(['source/img/icons/*.svg'])
+.pipe(svgo())
+.pipe(gulp.dest('build/img/icons/'));
+
+const svg2 = () =>
+gulp.src(['source/img/logos/*.svg'])
+.pipe(svgo())
+.pipe(gulp.dest('build/img/logos'));
+
+const sprite = () => {
+return gulp.src('source/img/sprite/*.svg')
+.pipe(svgo())
+.pipe(svgstore({
+inlineSvg: true
+}))
+.pipe(rename('sprite.svg'))
+.pipe(gulp.dest('build/img'));
+}
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+  'source/fonts/*.{woff2,woff}',
+  'source/*.ico',
+  'source/*.webmanifest',
+  ], {
+  base: 'source'
+  })
+  .pipe(gulp.dest('build'))
+  done();
+  }
+
+// Clean
+
+const clean = () => {
+  return del('build');
+  };
 
 // Server
 
 const server = (done) => {
   browser.init({
-    server: {
-      baseDir: 'source'
-    },
-    cors: true,
-    notify: false,
-    ui: false,
+  server: {
+  baseDir: 'build'
+  },
+  cors: true,
+  notify: false,
+  ui: false,
   });
   done();
-}
+  }
 
-// Watcher
+// Reload
 
-const watcher = () => {
+const reload = (done) => {
+  browser.reload();
+  done();
+  }
+
+  // Watcher
+
+  const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
-}
+  gulp.watch('source/js/script.js', gulp.series(scripts));
+  gulp.watch('source/*.html', gulp.series(html, reload));
+  }
 
+// Build
 
-export default gulp.series(
-  styles, server, watcher
-);
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  optimizeImages2,
+  gulp.parallel(
+  styles,
+  html,
+  scripts,
+  svg,
+  svg2,
+  sprite,
+  createWebp
+  ),
+  );
+
+  // Default
+
+  export default gulp.series(
+  clean,
+  copy,
+  copyImages,
+  copyImages2,
+  gulp.parallel(
+  styles,
+  html,
+  scripts,
+  svg,
+  svg2,
+  sprite,
+  createWebp
+  ),
+  gulp.series(
+  server,
+  watcher
+  ));
